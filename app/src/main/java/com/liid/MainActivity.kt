09aedb0dcb1191fb96ffeef34d6ff6b021c32d17
Renato <renatojobal@gmail.com>
2020-11-02 13:11:13 -0500

    feat: Set up navigation with bottom menu

--- app/src/main/java/com/liid/MainActivity.kt
+++ app/src/main/java/com/liid/MainActivity.kt
@@ -2,10 +2,20 @@ package com.liid
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setActionBar(findViewById(R.id.tool_bar_main))
+        configNav()
+    }
+
+    private fun configNav() {
+        NavigationUI.setupWithNavController(bottom_menu, Navigation.findNavController(this, R.id.fragment_content))
     }
 }

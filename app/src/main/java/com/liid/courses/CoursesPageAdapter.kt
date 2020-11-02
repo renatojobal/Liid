83b445b8450e48a04d1e0aa0f17d7718215d3639
Renato <renatojobal@gmail.com>
2020-11-02 14:37:10 -0500

    feat: Add a demo working page adapter for CoursesFragment.kt

---
+++ app/src/main/java/com/liid/courses/CoursesPageAdapter.kt
@@ -0,0 +1,28 @@
+package com.liid.courses
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentStatePagerAdapter
+
+private const val ARG_OBJECT = "object"
+
+class CoursesPageAdapter(fm: FragmentManager) : FragmentStatePagerAdapter(fm) {
+    override fun getCount(): Int {
+        return 7
+    }
+
+    override fun getItem(position: Int): Fragment {
+        val fragment = DemoObjectFragment()
+        fragment.arguments = Bundle().apply {
+            // Our object is just an integer :-P
+            putInt(ARG_OBJECT, position + 1)
+        }
+        return fragment
+    }
+
+    override fun getPageTitle(position: Int): CharSequence {
+        return "OBJECT ${(position + 1)}"
+    }
+
+}
